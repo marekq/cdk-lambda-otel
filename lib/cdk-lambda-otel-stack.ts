627bd2ebf104e0955fd73b533ec05410f24defaf
Marek Kuczynski <marek.kuczynski@stedi.com>
2022-07-05 00:08:09 +0200

    Added DynamoDB request tracing

--- lib/cdk-lambda-otel-stack.ts
+++ lib/cdk-lambda-otel-stack.ts
@@ -2,19 +2,34 @@ import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Architecture, Code, Function, LayerVersion, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class CdkLambdaOtelStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const dynamoDBTable = new Table(this, 'DynamoDBTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
+
     const lambdaFunction = new NodejsFunction(this, 'LambdaFunction', 
       {
         entry: './src/lambda/index.ts',
         runtime: Runtime.NODEJS_16_X,
-        timeout: Duration.seconds(15),
+        timeout: Duration.seconds(5),
         memorySize: 512,
+        reservedConcurrentExecutions: 1,
         tracing: Tracing.ACTIVE,
         architecture: Architecture.ARM_64,
+        bundling: {
+          minify: true,
+          forceDockerBundling: true
+        },
         layers: [
           LayerVersion.fromLayerVersionArn(
             this,
@@ -32,6 +47,10 @@ export class CdkLambdaOtelStack extends Stack {
           AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-handler",
           OPENTELEMETRY_COLLECTOR_CONFIG_FILE: "/opt/config.yaml"
         }
-      });
+      }
+    );
+
+    lambdaFunction.addEnvironment('DYNAMODB_TABLE_NAME', dynamoDBTable.tableName);
+    dynamoDBTable.grantReadWriteData(lambdaFunction);
   }
 }
--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,9 +1,21 @@
 import axios from 'axios';
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const AWS = new DynamoDBClient();
 
-exports.handler = async function (event) {
+exports.handler = async function (event: any) {
     console.log('request:', JSON.stringify(event, undefined, 2));
 
     const get = await axios.get('https://ipinfo.io/json');
+
+    const PutItem = new PutItemCommand({
+        TableName: process.env.DYNAMODB_TABLE_NAME,
+        Item: {
+            id: { S: Math.floor((Math.random() * 100) + 1).toString() },
+        }
+    });
+
+    const PutItemResponse = await AWS.send(PutItem);
+    console.log(PutItemResponse);
 
     return {
         statusCode: 200,
